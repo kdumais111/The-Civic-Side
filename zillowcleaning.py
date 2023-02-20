9fbbf19cb1a784f1c754c93c327b6c4d1576deed
Katherine Genis Dumais <kdumais@linux4.cs.uchicago.edu>
2023-02-20 17:52:52 -0600

    new data

--- zillowcleaning.py
+++ zillowcleaning.py
@@ -13,14 +13,17 @@
     fields.Zipcode = fields.Zipcode.str.replace(r"d{4}", r"0$&")
     fields.City = fields.City.str.lower()
     
-    Chicago_Zipcodes= [60601,60602,60603,60604,60605,60606,60607,60608,60609,60610,60611,60612,60613,60614,60615,60616,60617,60618,60619,60620,60621,60622,60623,60624,60625,60626,60628,60629,60630,60631,60632,60633,60634,60636,60637,60638,60639,60640,60641,60642,60643,60644,60645,60646,60647,60649,60651,60652,60653,60654,60655,60656,60657,60659,60660,60661,60706,60707,60827]
+    clean_zipfile= pathlib.Path("zillowcleaning.py").parent /"datasets/Chicago Precinct and Ward Names by Zip Code.csv"
+    clean_zip = pd.read_csv(clean_zipfile)
+    zip_clean=set(clean_zip["Zipcode"])
+    gov_zips = list(zip_clean)
     
-    for i, val in enumerate(Chicago_Zipcodes):
-        Chicago_Zipcodes[i]=str(val)
+    for i, val in enumerate(gov_zips):
+        gov_zips[i]=str(val)
     real_zip= set(fields.Zipcode[fields.City=="chicago"])
     
     missingzip= []
-    for val in Chicago_Zipcodes:
+    for val in gov_zips:
         if val not in real_zip:
             print(val)
             missingzip.append(str(val))
@@ -29,5 +32,8 @@
         fields.City[fields.Zipcode == missing]= "chicago"
     fields = fields.loc[fields.City == "chicago"]
     fields["2019avprice"]= fields.loc[:,["1/31/19","2/28/19","3/31/19","4/30/19","5/31/19","6/30/19","7/31/19","8/31/19","9/30/19","10/31/19","11/30/19","12/31/19"]].agg("mean",axis=1)
-    clean_data = fields[["zipcode","2019avprice"]]
-    clean_data.to_csv("zillow_cleaned_complete.csv")
+    clean_data = fields[["Zipcode","2019avprice"]]
+    clean_data.to_csv("datasets/zillow_cleaned_complete.csv")
+
+
+    #    Chicago_Zipcodes= [60601,60602,60603,60604,60605,60606,60607,60608,60609,60610,60611,60612,60613,60614,60615,60616,60617,60618,60619,60620,60621,60622,60623,60624,60625,60626,60628,60629,60630,60631,60632,60633,60634,60636,60637,60638,60639,60640,60641,60642,60643,60644,60645,60646,60647,60649,60651,60652,60653,60654,60655,60656,60657,60659,60660,60661,60706,60707,60827]
