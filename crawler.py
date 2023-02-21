fc063cfafebe7db95cd031f7883e845721b4aa92
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-02-21 00:03:16 -0600

    ID'd trouble rows

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
+import sys
 import lxml.html
 import json
-
 
 def get_contributions(saved_page):
     """
@@ -10,13 +10,8 @@ def get_contributions(saved_page):
 
     with open(saved_page) as sp:
         root = lxml.html.fromstring(sp.read())
-    
+     
     rows = root.cssselect("#ContentPlaceHolder1_gvContributions tr")
-    
-    header = rows[0]
-    col_names = []
-    for col in header:
-        col_names.append(col.text_content())
 
     del rows[0] # header
     del rows[-1] # footer
@@ -24,25 +19,37 @@ def get_contributions(saved_page):
     contributions = []
 
     for row in rows:
-        contribution = {}
-        cells = row.cssselect("td")
-        contribution["donor_name"] = cells[0].text
-        # .text only returns the first line of text if a cell has breaks
-        # .text_content() returns a lxml.etree._ElementUnicodeResult
-        # donor_info is name, address, and sometimes occupation and employer
-        # TODO: parse during cleaning
-        contribution["donor_info"] = str(cells[0].text_content()) 
-        contribution["amount"] = str(cells[1].text_content())
-        contribution["received_date"] = str(cells[2].text_content())
-        contribution["reported_date"] = str(cells[3].text_content())
-        contribution["contribution_type"] = cells[4].text
-        contribution["received_by"] = cells[4][1].text
-        contribution["description"] = str(cells[5].text_content())
-        contribution["vendor_name"] = str(cells[6].text_content())
-        contributions.append(contribution)
+            contribution = {}
+            cells = row.cssselect("td")
+            contribution["donor_name"] = cells[0].text
+            # .text only returns the first line of text if a cell has breaks
+            # .text_content() returns a lxml.etree._ElementUnicodeResult
+            # donor_info is name, address, and sometimes occupation and employer
+            # TODO: parse during cleaning
+            contribution["donor_info"] = str(cells[0].text_content()) 
+            contribution["amount"] = str(cells[1].text_content())
+
+            # TROUBLE ROWS
+            contribution["received_date"] = str(cells[2].text_content())
+            contribution["reported_date"] = str(cells[3].text_content())
+            contribution["contribution_type"] = cells[4].text
+            contribution["received_by"] = cells[4][1].text
+            contribution["description"] = str(cells[5].text_content())
+            contribution["vendor_name"] = str(cells[6].text_content())
+        
+            contributions.append(contribution)
 
     return contributions
 
+def save_contributions(contributions, file_name):
+    """
+    Takes a list of contribution dictionaries and saves them to a JSON file.
+
+    contributions: lst of strs
+    file_name: str
+    """
+    with open(file_name, "w"):
+        json.dumps(contributions)
 
 # Notes:
 # Not common, but a contribution can be described as a refund - 
