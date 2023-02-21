88f922a9db859969a395338f27c2ce3de95705f4
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-02-21 00:08:15 -0600

    Cleaning plan

---
+++ cleanup.py
@@ -0,0 +1,11 @@
+import sys
+import json
+import datetime
+
+# TODO list:
+# Delete name from donor_info (str.replace donor_name)
+# Exract occupation and employer from donor_info
+# Extract zip from donor_info
+# Create num_amount (remove "$" and "," from amount, case to float)
+# dates strs --> date objects
+    # https://www.educative.io/answers/how-to-convert-a-string-to-a-date-in-python
