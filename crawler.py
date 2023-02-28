43f8da226dd440975ae5d94749b61067139992de
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-02-28 15:20:56 -0600

    Updated and annotated

--- crawler.py
+++ crawler.py
@@ -1,43 +1,42 @@
-import sys
-import lxml.html
 import json
+import lxml.html
 
 def get_contributions(saved_page):
     """
-    Takes an html file saved from a committee contributions page and returns
+    Takes an HTML file saved from a committee contributions page and returns
     a list of dictionaries (one dictionary per contribution).
     """
 
     with open(saved_page) as sp:
         root = lxml.html.fromstring(sp.read())
-     
+
     rows = root.cssselect("#ContentPlaceHolder1_gvContributions tr")
 
     del rows[0] # header
-    del rows[-1] # footer
+    del rows[-2:] # page numbers
 
     contributions = []
 
     for row in rows:
-            contribution = {}
-            cells = row.cssselect("td")
-            contribution["donor_name"] = cells[0].text
-            # .text only returns the first line of text if a cell has breaks
-            # .text_content() returns a lxml.etree._ElementUnicodeResult
-            # donor_info is name, address, and sometimes occupation and employer
-            # TODO: parse during cleaning
-            contribution["donor_info"] = str(cells[0].text_content()) 
-            contribution["amount"] = str(cells[1].text_content())
+        contribution = {}
+        cells = row.cssselect("td")
+        contribution["donor_name"] = cells[0].text
+        # .text only returns the first line of text if a cell has breaks
+        # .text_content() returns a lxml.etree._ElementUnicodeResult
+        # donor_info is name, address, and sometimes occupation and employer
+        # clean() in cleanup.py deletes name and extracts address
+        # TODO: decide whether to extract occupation and employer, too
+        contribution["donor_info"] = str(cells[0].text_content())
+        contribution["amount"] = str(cells[1].text_content())
+        contribution["received_date"] = str(cells[2].text_content())
+        contribution["reported_date"] = str(cells[3].text_content())
+        contribution["contribution_type"] = cells[4].text
+        contribution["received_by"] = cells[4][1].text
+        contribution["description"] = str(cells[5].text_content())
+        # TODO if time: figure out why cells[6].text_content() returns unicode space characters?
+        contribution["vendor_name"] = str(cells[6].text_content())
 
-            # TROUBLE ROWS
-            contribution["received_date"] = str(cells[2].text_content())
-            contribution["reported_date"] = str(cells[3].text_content())
-            contribution["contribution_type"] = cells[4].text
-            contribution["received_by"] = cells[4][1].text
-            contribution["description"] = str(cells[5].text_content())
-            contribution["vendor_name"] = str(cells[6].text_content())
-        
-            contributions.append(contribution)
+        contributions.append(contribution)
 
     return contributions
 
@@ -45,12 +44,13 @@ def save_contributions(contributions, file_name):
     """
     Takes a list of contribution dictionaries and saves them to a JSON file.
 
-    contributions: lst of strs
-    file_name: str
+    Inputs:
+        contributions: lst of strs
+        file_name: str
     """
-    with open(file_name, "w"):
-        json.dumps(contributions)
+    with open(file_name, "w") as f:
+        json.dump(contributions, f, indent=4)
 
 # Notes:
-# Not common, but a contribution can be described as a refund - 
+# Not common, but a contribution can be described as a refund -
 # what does that mean in this context?
