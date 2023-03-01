1ddf6451e3f3791993076bdb588dea7dfefa0c32
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-03-01 16:11:21 -0600

    Created file

---
+++ utils.py
@@ -0,0 +1,8 @@
+START = "11/26/2018" # Start of 2019 candidate filing period
+END = "12/31/2019" # End of 2019 election cycle
+CHI_ZIPS = []
+
+# References (relevant dates):
+# Illinois State Board of Elections 2019 Election and Campaign Finance Calendar
+# available at https://www.elections.il.gov/Main/Publications.aspx
+# 10 ILCS 5/9-1.9 available at https://www.ilga.gov/legislation/ilcs/fulltext.asp?DocName=001000050K9-1.9
