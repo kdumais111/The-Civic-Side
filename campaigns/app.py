7bf7d4281ff1768de019753a6f5b836dd23eb4ba
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-03-04 12:04:49 -0600

    Running from command line

--- campaigns/app.py
+++ campaigns/app.py
@@ -1,17 +1,18 @@
 from .crawler import get_contributions, save_contributions
 from .cleanup import clean, merge_candidates, process_contributions
 from .stats import zip_stats, chi_stats
-from .utils import SAVED_PAGES, START, END, ZIP_STRS
+from .utils import PAGES_TO_SCRAPE, START, END, ZIP_STRS
 
+# page_file --> raw_data
 def run():
     clean_files = []
-    for page, page_file in SAVED_PAGES:
+    for page, raw_data in PAGES_TO_SCRAPE:
         contributions = get_contributions(page)
-        save_contributions(contributions, page_file)
-        clean(page_file)
-        clean_files.append(page_file.split(".")[0] + "-clean.json")
+        save_contributions(contributions, raw_data)
+        clean(raw_data)
+        clean_files.append(raw_data.split(".")[0] + "_clean.json")
     contributions = merge_candidates(clean_files)
     processed_contributions = process_contributions(contributions, START, END)
-    zip_stats(process_contributions, ZIP_STRS, "stats-by-zip.json") # TODO: Update file name / path
-    chi_stats("stats-by-zip.json", "citywide-stats.json") # TODO: Update file name / path
-
+    zip_stats(processed_contributions, ZIP_STRS, "campaigns/contributions/stats_by_zip.json")
+    chi_stats("campaigns/contributions/stats_by_zip.json", 
+              "campaigns/contributions/city_stats.json")
