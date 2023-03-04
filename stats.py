a0648bfaa051551ead82c25b3280ec906c3cfce1
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-03-03 21:38:04 -0600

    Moved processing functions to clean.py, defined chi_stats function

--- stats.py
+++ stats.py
@@ -2,54 +2,7 @@ import pandas as pd
 import json
 import datetime
 
-def merge_candidates(candidates):
-    """
-    Takes a list of clean campaign contributions data filenames (one per candidate)
-    and returns a dataframe with contributions for all of the specified candidates.
-
-    Inputs:
-        candidates (lst of file names): e.g., ["candidate1_clean.json", "candidate2_clean.json"]
-
-    Returns:
-        contributions (Pandas data frame)
-    """
-    dfs = []
-    for candidate in candidates:
-        df = pd.read_json(candidate, dtype=False)
-        dfs.append(df)
-    
-    contributions = pd.concat(dfs)
-
-    return contributions
-
-
-def process_contributions(df, period_start, period_end):
-    """
-    Takes a Pandas dataframe of clean campaign contributions data (df),
-    loads it into a Pandas data frame, converts dates from strings,
-    and drops contributions received outside the specified period.
-
-    Inputs:
-        json_file: clean candidate contributions data
-        period_start (str): "MM/DD/YYYY"
-        period_end (str): "MM/DD/YYYY"
-
-    Returns:
-        df (Pandas dataframe): campaign contributions from the specified period
-    """
-    start = pd.to_datetime(period_start)
-    end = pd.to_datetime(period_end)
-
-    df["received_date"] = pd.to_datetime(df["received_date"])
-    df["zip_num"] = df["zip"].astype("Int64")
-    outside_period = df[ (df["received_date"] < start) | 
-        (df["received_date"] > end) ].index
-    df.drop(outside_period, inplace=True)
-    
-    return df
-
-
-def summary_stats(df, zips, stats_file):
+def zip_stats(df, zips, stats_file):
     """
     Genereates summary statistics for the specified zip codes and
     saves them to a JSON file.
@@ -81,11 +34,25 @@ def summary_stats(df, zips, stats_file):
     with open(stats_file, "w") as sf:
         json.dump(stats, sf, indent=1)
 
+def chi_stats(stats_file, chi_file)
+    """
+    Takes a JSON file of statistics by Chicago zip code (stats_file), aggregates
+    the statistics to the city level, and saves the aggregated data to a JSON file
+    (chi_file).
+    """
+    with open(stats_file) as sf:
+        by_zip = json.load(sf)
 
-# References (process_contributions):
-# https://sparkbyexamples.com/pandas/pandas-change-string-object-to-date-in-dataframe/
-# https://www.geeksforgeeks.org/drop-rows-from-the-dataframe-based-on-certain-condition-applied-on-a-column/
-# https://stackoverflow.com/questions/36921951/truth-value-of-a-series-is-ambiguous-use-a-empty-a-bool-a-item-a-any-o
+    chi = {}
+    chi["num_donations"] = by_zip["num_donations"].sum()
+    chi["total_donated"] = by_zip["total_donated"].sum()
+    chi["min_donation"] = by_zip["min_donation"].min()
+    chi["max_donation"] = by_zip["max_donation"].min()
+    chi["avg_donation"] = (chi["total_donated"] / chi["num_dunations"])
+
+    with open(chi_file, "w") as cf:
+        json.dump(chi, cf, indent=1)
+
 
 # References (summary_stats):
 # https://stackoverflow.com/questions/13784192/creating-an-empty-pandas-dataframe-and-then-filling-it
