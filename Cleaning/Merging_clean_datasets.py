7b08f29d579dd89fa023ab376aca3cf212e4de80
Katherine Genis Dumais <kdumais@linux4.cs.uchicago.edu>
2023-03-04 02:36:36 -0600

    Clean function complete

--- Cleaning/Merging_clean_datasets.py
+++ Cleaning/Merging_clean_datasets.py
@@ -1,35 +1,100 @@
 import pandas as pd
 import pathlib
 
-#combine voting datasets and zipcode
-districts = pathlib.Path("Merging_clean_datasets.py"
-                    ).parent /"Cleaning/clean_zipcode_precinct.csv"
-voters= pathlib.Path("Merging_clean_datasets.py").parent /"Cleaning/voterturnout.csv"
-voting_df = pd.read_csv(voters)
-districts_df = pd.read_csv(districts)
-voting_df["precinct"]=voting_df["precinct"].astype(int)
-voting_district = pd.merge(districts_df, voting_df, 
-                on=["ward","precinct"], how='inner')
-#voting_district=voting_district[["zip", "precinct","ward","Turnout"]]
-voting_combined = voting_district.groupby(["zip"]).sum().reset_index()
-voting_combined["votingrates"]= voting_district["Ballots Cast"]/voting_district["Register Voters"]
-voting_combined = voting_combined[['zip', "votingrates"]]
+from ._311_Dataset_Cleaning import create_311_clean_csvs
+from .cleanzipcodes_toprecincts import build_zip_precinct_csv
+from .voterturnout_cleaning import clean_voter_turnout
+from .zillowcleaning import clean_zillow_to_csv
 
-#combine zillow dataset- housing prices
-zillow= pathlib.Path("Merging_clean_datasets.py"
-                ).parent /"Cleaning/zillow_cleaned_complete.csv"
-housing = pd.read_csv(zillow)
-voting_housing = pd.merge(voting_combined,housing, left_on="zip", 
-                right_on="Zipcode", how="left").drop(columns = ['Zipcode'])
+#Code written by Katherine Dumais
 
-# add in complaint counts
-complaints= pathlib.Path("Merging_clean_datasets.py"
-                ).parent /"Cleaning/311_complaint_count.csv"
-complaints = pd.read_csv(complaints)
-add_complaints= pd.merge(voting_housing, complaints, left_on="zip", 
-                right_on="zipcode", how="left").drop(columns = ['zipcode', 'Unnamed: 0'])
 
-# Make csv
-pd.to_csv(add_complaints)
+def execute_data_merge():
+    '''
+    Clean all non-campaign finance data and merge it together.
+    '''
+    #Create Clean Datasets
+    create_311_clean_csvs()
+    #build_zip_precinct_csv()
+    clean_voter_turnout()
+    clean_zillow_to_csv()
+   
+
+    #Combine datasets
+    voting_zipcode = voting_to_zipcode()
+    zillow= pathlib.Path(__file__
+                ).parent /"zillow_cleaned_complete.csv"
+    voting_housing = combine_data_zip(voting_zipcode, zillow, "Zipcode")
+    complaints= pathlib.Path(__file__
+                ).parent /"311_complaint_count.csv"
+    add_complaints = combine_data_zip(voting_housing, complaints, "zipcode")
+
+    # Make csv
+    add_complaints.to_csv("merged.csv", index=False)
+    return print("data clean and merge complete")
+
+def voting_to_zipcode():
+    '''
+    Import Voting Turnout CSV and Zipcode to Precinct CSV to dataframe.
+    Combine datasets by zipcode.
+    '''
+    districts = pathlib.Path(__file__
+                        ).parent /"clean_zipcode_precinct.csv"
+    voters= pathlib.Path(__file__).parent /"voterturnout.csv"
+    voting_df = pd.read_csv(voters)
+    districts_df = pd.read_csv(districts)
+    voting_df["precinct"]=voting_df["precinct"].astype(int)
+    voting_district = pd.merge(districts_df, voting_df, 
+                    on=["ward","precinct"], how='inner')
+    voting_combined = voting_district.groupby(["zip"]).sum().reset_index()
+    voting_combined["votingrates"]= voting_district["Ballots Cast"]\
+                                    / voting_district["Register Voters"]
+    voting_combined = voting_combined[['zip', "votingrates"]]
+    return voting_combined
+
+
+def combine_data_zip(df, filename, column):
+    '''
+    Takes a dataframe and adds data from a CSV, joining by zipcode.
+    Returns merged dataframe.
+    '''
+    new_data = pd.read_csv(filename)
+    merged_set = pd.merge(df, new_data, left_on = "zip", 
+                    right_on = column, how = "left").drop(columns = [column])
+    merged_set = merged_set.dropna()
+    return merged_set
+
+
+
+
+
+# def combine_zillow_data(df):
+#     '''
+#     Takes a dataframe and combines the 2019 average housing price data csv
+#     ("zillow_cleaned_complete.csv") and adds it to the dataframe by zip.
+#     '''
+#     #combine zillow dataset- housing prices
+#     zillow= pathlib.Path("Merging_clean_datasets.py"
+#                     ).parent /"Cleaning/zillow_cleaned_complete.csv"
+#     housing = pd.read_csv(zillow)
+#     voting_housing = pd.merge(df,housing, left_on = "zip", 
+#                     right_on = "Zipcode", how = "left").drop(columns = ['Zipcode'])
+#     return voting_housing
+
+# def combine_complaints_data(df):
+#     '''
+#     Combine Count of Complaints by Dataset into the CSV 
+#     '''
+#     complaints= pathlib.Path("Merging_clean_datasets.py"
+#                     ).parent /"Cleaning/311_complaint_count.csv"
+#     complaints = pd.read_csv(complaints)
+#     add_complaints = pd.merge(df, complaints,
+#                             left_on = "zip", right_on = "zipcode", how = "left"
+#                             ).drop(columns = ['zipcode', 'Unnamed: 0'])
+#     add_complaints = add_complaints.dropna()
+#     return add_complaints
+
+
+
 
 
--- Cleaning/zillowcleaning.py
+++ Cleaning/zillowcleaning.py
@@ -1,37 +1,83 @@
 import pandas as pd
 import pathlib
-filename = pathlib.Path("zillowcleaning.py").parent /"Datasets/ZillowHousing.csv"
-df = pd.read_csv(filename)
 
-fields = df[["State", "City", "CountyName","RegionName","1/31/19","2/28/19","3/31/19","4/30/19","5/31/19","6/30/19","7/31/19","8/31/19","9/30/19","10/31/19","11/30/19","12/31/19"]]
-fields.State= fields.State.astype("string")
-fields.State = fields.State.str.lower()
-fields= fields.loc[fields.State == 'il']
-fields= fields.loc[fields.CountyName == 'Cook County']
-fields= fields.rename(columns={'RegionName': 'Zipcode'})
-fields.Zipcode = fields.Zipcode.astype("string")
-fields.Zipcode = fields.Zipcode.str.replace(r"d{4}", r"0$&")
-fields.City = fields.City.str.lower()
+#Written by Katherine Dumais
 
-clean_zipfile= pathlib.Path("zillowcleaning.py").parent /"Datasets/Chicago Precinct by Zip Code.csv"
-clean_zip = pd.read_csv(clean_zipfile)
-zip_clean=set(clean_zip["Zipcode"])
-gov_zips = list(zip_clean)
+def clean_zillow_to_csv():
+    '''
+    Runs all functions used to build a clean zillow datasets
+    Outputs a CSV with related data
+    '''
+    fields = clean_zillow_data()
+    mapped_fields = map_city_data(fields)
+    complete_data = housingprice_mean_2019(mapped_fields)
+    filepath = pathlib.Path(__file__).parent /"zillow_cleaned_complete.csv"
+    complete_data.to_csv(filepath, index=False)
+    return print("Zillow CSV Complete")
 
-for i, val in enumerate(gov_zips):
-    gov_zips[i]=str(val)
-real_zip= set(fields.Zipcode[fields.City=="chicago"])
 
-missingzip= []
-for val in gov_zips:
-    if val not in real_zip:
-        print(val)
-        missingzip.append(str(val))
+def clean_zillow_data():
+    '''
+    Load zillow housingdata as a pandas database. 
+    Segment to just 2019 data and to the City of Chicago
+    Returns database of this data
+    '''
+    filename = pathlib.Path(__file__).parent /"Datasets/ZillowHousing.csv"
+    df = pd.read_csv(filename)
+    fields = df[["State", "City", "CountyName","RegionName", "1/31/19",
+                 "2/28/19", "3/31/19", "4/30/19", "5/31/19", "6/30/19",
+                 "7/31/19", "8/31/19", "9/30/19", "10/31/19",
+                 "11/30/19", "12/31/19"]]
+    fields.State = fields.State.astype("string")
+    fields.State = fields.State.str.lower()
+    fields = fields.loc[fields.State == 'il']
+    fields = fields.loc[fields.CountyName == 'Cook County']
+    fields = fields.rename(columns = {'RegionName': 'Zipcode'})
+    fields.Zipcode = fields.Zipcode.astype("string")
+    fields.Zipcode = fields.Zipcode.str.replace(r"d{4}", r"0$&")
+    fields.City = fields.City.str.lower()
+    return fields 
 
-for missing in missingzip:
-    fields.City[fields.Zipcode == missing]= "chicago"
-fields = fields.loc[fields.City == "chicago"]
-fields["2019avprice"]= fields.loc[:,["1/31/19","2/28/19","3/31/19","4/30/19","5/31/19","6/30/19","7/31/19","8/31/19","9/30/19","10/31/19","11/30/19","12/31/19"]].agg("mean",axis=1)
-clean_data = fields[["Zipcode","2019avprice"]]
-clean_data.to_csv("zillow_cleaned_complete.csv", index=False)
+
+def map_city_data(df):
+    '''
+    Imports a list of official zipcodes from the City of Chicago, matches
+    the zipcodes in a dataframe to the official list.
+    Takes any official zipcodes that may have a different city name listed
+    reclassifies them to chicago and returns dataframe segmented to Chicago.
+    '''
+    clean_zipfile = pathlib.Path(__file__
+                        ).parent /"Datasets/Chicago Precinct by Zip Code.csv"
+    clean_zip = pd.read_csv(clean_zipfile)
+    zip_clean = set(clean_zip["Zipcode"])
+    gov_zips = list(zip_clean)
+
+    for i, val in enumerate(gov_zips):
+        gov_zips[i] = str(val)
+    real_zip = set(df.Zipcode[df.City == "chicago"])
+
+    missingzip = []
+    for val in gov_zips:
+        if val not in real_zip:
+            missingzip.append(str(val))
+    for missing in missingzip:
+        df.City[df.Zipcode == missing] = "chicago"
+    df = df.loc[df.City == "chicago"]
+    return df
+
+
+def housingprice_mean_2019(df):
+    '''
+    Take all 2019 median housing prices and average them, to avoid 
+    impact of outliers.
+    Returns dataframe of just zipcodes and average housing prices. 
+    '''
+    df["2019avprice"] = df.loc[:,["1/31/19", "2/28/19", "3/31/19",
+                                        "4/30/19", "5/31/19", "6/30/19", 
+                                        "7/31/19","8/31/19", "9/30/19",
+                                        "10/31/19", "11/30/19", "12/31/19"]
+                                        ].agg("mean",axis=1)
+    complete_data = df[["Zipcode","2019avprice"]]
+    return complete_data
+
 
