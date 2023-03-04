f978be23c891fe56565f14d81359a4b4ec6d6a8d
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-03-03 23:35:51 -0600

    Drafted 'run' function

--- campaigns/app.py
+++ campaigns/app.py
@@ -1,9 +1,17 @@
 from .crawler import get_contributions, save_contributions
-from .utils import SAVED_PAGES
+from .cleanup import clean, merge_candidates, process_contributions
+from .stats import zip_stats, chi_stats
+from .utils import SAVED_PAGES, START, END, ZIP_STRS
 
 def run():
-    for page, destination in SAVED_PAGES:
+    clean_files = []
+    for page, page_file in SAVED_PAGES:
         contributions = get_contributions(page)
-        save_contributions(contributions, destination)
+        save_contributions(contributions, page_file)
+        clean(page_file)
+        clean_files.append(page_file.split(".")[0] + "-clean.json")
+    contributions = merge_candidates(clean_files)
+    processed_contributions = process_contributions(contributions, START, END)
+    zip_stats(process_contributions, ZIP_STRS, "stats-by-zip.json") # TODO: Update file name / path
+    chi_stats("stats-by-zip.json", "citywide-stats.json") # TODO: Update file name / path
 
-    print("It worked!")
