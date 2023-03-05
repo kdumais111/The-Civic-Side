a250117df78de48e82d1fa8d9212ca0dcf7be554
Katherine Genis Dumais <kdumais@linux4.cs.uchicago.edu>
2023-03-04 21:11:53 -0600

    hello

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,20 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 import geopandas as gpd
+from urllib.request import urlopen
+import json
 
 # Figures are represented as trees with named nodes called "attributes". 
 # The root node of the tree has three top-level attributes: data, layout and frames.
 
-app = Dash()
+#Import idea taken from Stephania and Project APWhy
+zip_url = "https://data.cityofchicago.org/api/geospatial/gdcf-axmw?method=export&format=GeoJSON"
+with urlopen(zip_url) as response:
+    zipcodes = json.load(response)
+
+app = Dash(external_stylesheets=[dbc.themes.YETI])
 
 df = pd.read_csv("merged.csv")
 shape_file = "Cleaning/Datasets/zip_geojson.geojson"
@@ -17,11 +24,11 @@ shape_file = "Cleaning/Datasets/zip_geojson.geojson"
 #                     color="votingrates", 
 #                     projection="mercator")
 
-fig = px.choropleth_mapbox(df, geojson=shape_file, locations="zipcode", featureidkey="properties.zip",
-                           color="votingrates", mapbox_style="carto-positron", center={"lat":41.8, "lon": -87.75}
+
+
+fig = px.choropleth_mapbox(df, geojson=zipcodes, locations="zip", featureidkey="properties.zip",
+                           color="votingrates", mapbox_style="carto-positron", center={"lat":41.8, "lon": -87.75, }
                    )
-
-
 
 fig.update_geos(fitbounds="locations", visible=False)
 
@@ -31,6 +38,9 @@ button1 =  dict(method = "restyle",
 button2 =  dict(method = "restyle",
                 args = [{'z': [ df["complaintcounts"] ]}],
                 label = "3-1-1 Complaints")
+button3 =  dict(method = "restyle",
+                args = [{'z': [ df["2019avprice"] ]}],
+                label = "Average Housing Prices")
 
 fig.update_layout(width=700,
                   coloraxis_colorbar_thickness=23,
@@ -40,17 +50,33 @@ fig.update_layout(width=700,
                                     xanchor='right',
                                     yanchor='top',
                                     active=0,
-                                    buttons=[button1, button2],
+                                    buttons=[button1, button2, button3],
                                     )
                               ])
 
 app.layout = html.Div(
-    [html.H1("THE CIVIC SIDE"), dcc.Graph(figure=fig)]
+    [html.H1("THE CIVIC SIDE"),
+     html.H3("By Katherine Dumais and Francesca Vescia"),
+     html.P("We began this project to look at..."),
+     dbc.Row([dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown')]),
+     dbc.Row(
+    [
+    dbc.Col(
+        dcc.Graph(id="1", figure=fig)),
+    dbc.Col(
+        dcc.Graph(id="2", figure=fig))
+    ]),
+    dbc.Row(),
+     ]
 )
+#@app.callback(
+#    Output('dd-output-container', 'children'),
+#    Input('demo-dropdown', 'value')
+#)
+#def update_table():
+#    pass
 
-
-
-app.run_server(debug=True)
+app.run_server(debug=False)
 
 # https://plotly.com/python/choropleth-maps/#using-geopandas-data-frames
 # https://plotly.com/python/mapbox-county-choropleth/
