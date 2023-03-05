b55d30e4e3801b65c5715f5054a4ba48c2bb3485
Francesca Vescia <fvescia@Francescas-MacBook-Air.local>
2023-03-05 10:02:30 -0600

    Updated file structure

---
+++ civic-side/the-polis/Merging_clean_datasets.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import pathlib
+
+from ._311_Dataset_Cleaning import create_311_clean_csvs
+from .cleanzipcodes_toprecincts import build_zip_precinct_csv
+from .voterturnout_cleaning import clean_voter_turnout
+from .zillowcleaning import clean_zillow_to_csv
+
+#Code written by Katherine Dumais
+
+
+def execute_data_merge():
+    '''
+    Clean all non-campaign finance data and merge it together.
+    '''
+    #Create Clean Datasets
+    create_311_clean_csvs()
+    build_zip_precinct_csv()
+    clean_voter_turnout()
+    clean_zillow_to_csv()
+   
+
+    #Combine datasets
+    voting_zipcode = voting_to_zipcode()
+    zillow= pathlib.Path(__file__
+                ).parent /"zillow_cleaned_complete.csv"
+    voting_housing = combine_data_zip(voting_zipcode, zillow, "Zipcode")
+    complaints= pathlib.Path(__file__
+                ).parent /"311_complaint_count.csv"
+    add_complaints = combine_data_zip(voting_housing, complaints, "zipcode")
+    campaign = pathlib.Path(__file__
+                ).resolve().parents[1] /"campaigns/contributions/stats_by_zip.json"
+    add_campaign = combine_data_zip(add_complaints, campaign, "zip")
+    complete = add_campaign.drop(add_complaints.columns[3], axis=1)
+
+    # Make csv
+    complete.to_csv("merged.csv", index=False)
+    return print("data clean and merge complete")
+
+def voting_to_zipcode():
+    '''
+    Import Voting Turnout CSV and Zipcode to Precinct CSV to dataframe.
+    Combine datasets by zipcode.
+    '''
+    districts = pathlib.Path(__file__
+                        ).parent /"clean_zipcode_precinct.csv"
+    voters = pathlib.Path(__file__).parent /"voterturnout.csv"
+    voting_df = pd.read_csv(voters)
+    districts_df = pd.read_csv(districts)
+    voting_df["precinct"] = voting_df["precinct"].astype(int)
+    voting_district = pd.merge(districts_df, voting_df, 
+                    on=["ward","precinct"], how='inner')
+    voting_combined = voting_district.groupby(["zip"]).sum().reset_index()
+    voting_combined["votingrates"]= voting_district["Ballots Cast"]\
+                                    / voting_district["Register Voters"]
+    voting_combined = voting_combined[['zip', "votingrates"]]
+    return voting_combined
+
+
+def combine_data_zip(df, filename, column):
+    '''
+    Takes a dataframe and adds data from a CSV, joining by zipcode.
+    Returns merged dataframe.
+    '''
+    if "json" in str(filename):
+        new_data= pd.read_json(filename)
+    else:
+        new_data = pd.read_csv(filename)
+    if column == "zip":
+        merged_set = pd.merge(df, new_data, how='left', on="zip")   
+    else:
+        merged_set = pd.merge(df, new_data, left_on = "zip", 
+                        right_on = column, how = "left").drop(columns = [column])
+    merged_set = merged_set.dropna()
+    return merged_set
+
+
+
+
+
---
+++ civic-side/the-polis/_311_Dataset_Cleaning.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import pathlib
+
+#Code Written By Katherine Dumais
+
+def create_311_clean_csvs():
+    '''
+    Creates a CSV for complaint counts per 1,000 people by zipcode
+    and a CSV documenting the top complaints for each zipcode using
+    the related functions below
+    '''
+    df = make_311dataframe()
+    complaints = complaint_counts(df)
+    population_vals = read_clean_pop_data()
+    complaints_csv(complaints, population_vals)
+    find_top_five(df, population_vals)
+
+
+def make_311dataframe():
+    '''
+    Downloads and filters the 311 data to solely look at location and 
+    complaint type.
+    Returns filtered dataset
+    '''
+    # Data downloaded exclusively for 2019 
+    df = pd.read_csv("https://30122-public.s3.amazonaws.com/311_2019.csv")
+    df = df[["SR_TYPE", "ZIP_CODE", "WARD", "PRECINCT"]]
+    df["ZIP_CODE"] = pd.to_numeric(df["ZIP_CODE"], errors = 'coerce')
+    return df
+
+
+def complaint_counts(df):
+    '''
+    Finds the count of 311 complaints by zipcode
+    Returns a dataframe of counts by zipcode.
+    '''
+    zip = df["ZIP_CODE"].value_counts(dropna=True).to_frame('complaintcounts'
+                                                            ).reset_index()
+    zip = zip.rename(columns = {'index':'zipcode'})
+    zip["zipcode"] = zip["zipcode"].astype(int)
+    return zip
+
+
+def read_clean_pop_data():
+    '''
+    Reads and cleans a set of population data by zipcode provided by the city. 
+    Returns a dataframe of zipcodes and population numbers.
+    '''
+    # Import population numbers for 2019 
+    f = pathlib.Path(__file__).parent /"Datasets/Chicago_Population_Counts.csv"
+    pop = pd.read_csv(f)
+    pop = pop[pop['Year'] == 2019]
+
+    # Make into integers and clean out non-number characters
+    # Geography is what 311 calls zipcode.
+    pop = pop[['Geography','Population - Total']]
+    pop['Geography'] = pd.to_numeric(pop['Geography'], errors = 'coerce')
+    pop['Population - Total'] = pop['Population - Total'].str.replace(
+        '[^0-9]', '', regex=True).astype('int64')
+    pop = pop.dropna()
+    pop['Geography'] = pop['Geography'].astype(int)
+    pop = pop.rename(columns = {'Geography':"zipcode"})
+    return pop
+
+
+def complaints_csv(zip, pop):
+    '''
+    Takes population to zipcode and 311 complaint count data.
+    - Narrows 311 zipcodes to exclusively Chicago/ Cleans out typo'ed zipcodes.
+    - Merges dataframes
+    - Finds Complaint Counts per 100,000 people
+    - Creates a CSV for complaint counts 
+    '''
+
+    zip = zip[zip['zipcode'].isin (pop['zipcode'])]
+    zip = zip.merge(pop, how = 'left', on = 'zipcode')
+    zip['per1000_complaint'] = (zip['complaintcounts'] /
+                                zip["Population - Total"] * 1000)
+    
+    zip.to_csv(pathlib.Path(__file__).parent /'_311_complaint_count.csv',
+                index= False)
+    return print('311 data csv created')
+
+def find_top_five(df, pop):
+    '''
+    Uses 311 dataframe (df) and dataframe of population to zipcode (pop) to
+    find top 5 complaints by zipcode. Creates a CSV of top 5 complaints by zip.
+    '''
+    df = df.dropna()
+    df["ZIP_CODE"] = df["ZIP_CODE"].astype(int)
+    df = df[df["ZIP_CODE"].isin (pop['zipcode'])]
+    comp_by_zip = []
+    for _ , zipcodegroup in df[["SR_TYPE","ZIP_CODE"]].groupby("ZIP_CODE"):
+        comp_by_zip.append(zipcodegroup.value_counts(
+        sort = True, dropna = True).to_frame('complaintcounts').iloc[0:5])
+    complaints = pd.concat(comp_by_zip) 
+    complaints.to_csv(pathlib.Path(__file__).parent /'311_topcomplaints_byzip.csv'\
+                      , header = True)
+    return print( '311_top complaints data created')
+
+
